7ebfd77ea9d8cc33b2d2ac68f5e43bf7a044e6a9
sirsa4 <yaya.abbdi@gmail.com>
2024-07-29 02:57:08 +0200

    slideshow added

--- src/app/components/projects/Dialog.tsx
+++ src/app/components/projects/Dialog.tsx
@@ -1,31 +1,56 @@
 
 "use client"
 import { ProjectType } from '@/app/utils/type'
-import { useRef } from 'react'
 import { FaGithubSquare } from "react-icons/fa"
 import DialogTitle from './DialogTitle'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
+import { useState } from 'react'
 const Dialog = ({ img, url, title, text, github, dialogRef, close, slide, id }: ProjectType) => {
+  const [src, setSrc] = useState<null | string>(slide ? slide[0] : null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const next = () => {
+    setCurrentIndex((prevIndex) => {
+      let newIndex = (prevIndex + 1) % slide?.length;
+      setSrc(slide ? slide[newIndex] : null);
+      return newIndex;
+    })
+  }
+  const prev = () => {
+    setCurrentIndex((previousIndex) => {
+      let prevIndex = (previousIndex - 1) % slide?.length;
+      setSrc(slide ? slide[prevIndex] : null);
+      return prevIndex;
+    })
+  }
   return (
     <dialog ref={dialogRef} className="w-full">
       <article className="bg-primary-white rounded-lg shadow-md hover:shadow-2xl duration-300">
         <DialogTitle close={close} />
-        <div className="p-4 grid grid-cols-2">
+        <div className="p-4 border-b grid grid-cols-2">
           <div className="capitalize p-8">
             <h2 className="text-6xl tracking-wide font-medium">{title}</h2>
             <p className="mt-4 text-slate-700 leading-loose">{text}</p>
-            <div className="mt-4">
-              <button
-                type="button"
-                className="bg-primary-dark text-primary-white hover:bg-accent font-semibold py-2 px-4 rounded transition duration-300 hidden md:block"
-                onClick={close}
-              >
-                Close
-              </button>
-            </div>
           </div>
           <img src={img} alt={title} className="w-full object-cover rounded-t-lg h-64" />
-          <p>slide: {slide ? slide[0] : "no slide images"}</p>
         </div>
+        <section className="p-4 w-1/2">
+          <p>slide: {slide ? src : "no slide images"}</p>
+          <img src={src} alt="slide image" className="rounded" />
+          <div className="py-4 flex justify-center gap-x-10">
+            <ArrowLeft onClick={prev} className="cursor-pointer" />
+            <ArrowRight onClick={next} className="cursor-pointer" />
+          </div>
+          <div className="mt-4">
+            <button
+              type="button"
+              className="bg-primary-dark text-primary-white hover:bg-accent font-semibold py-2 px-4 rounded transition duration-300 hidden md:block"
+              onClick={close}
+            >
+              Close
+            </button>
+          </div>
+        </section>
+
       </article>
     </dialog>
   )
--- src/app/components/projects/DialogTitle.tsx
+++ src/app/components/projects/DialogTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DialogTitle = ({ close }: { close: () => void }) => {
   return (
-    <div className="py-4 p-4 bg-primary-light flex justify-end">
+    <div className="py-4 p-4 bg-primary-light flex justify-end sticky">
       <button
         type="button"
         className="bg-primary-dark text-primary-white hover:bg-accent font-semibold py-2 px-4 rounded transition duration-300 hidden md:block"
