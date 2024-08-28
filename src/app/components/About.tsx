80d3436a90e6fc1ac5ea2a0375e103ef8bdf4ac4
sirsa4 <yaya.abbdi@gmail.com>
2024-08-28 18:08:09 +0200

    about img fix

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Title from './Title'
+import Image from 'next/image'
 
 const About = () => {
 
   return (
     <section className="bg-primary py-20" id="about">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
-        <img src="/assets/about.svg" alt="image of a person" className="w-full h-80" />
+        <Image
+          src="/assets/about.svg"
+          alt="image of a person"
+          width={300} // Replace with your desired width
+          height={300} // Replace with your desired height
+          className="w-full h-80" // Optional, keep if needed
+        />
         <article>
           <Title text="About me" />
           <p className="text-slate-600 mt-8 leading-loose">
