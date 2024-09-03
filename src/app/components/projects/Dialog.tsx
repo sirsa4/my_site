7db91ece8f35df6434e6c9426a5be1ed6f5e5c78
sirsa4 <yaya.abbdi@gmail.com>
2024-09-03 19:33:56 +0200

    some types in Dialog.tsx fixed

--- src/app/components/projects/Dialog.tsx
+++ src/app/components/projects/Dialog.tsx
@@ -1,11 +1,21 @@
-
-"use client"
-import { ProjectType } from '@/app/utils/type'
-import { FaGithubSquare } from "react-icons/fa"
-import DialogTitle from './DialogTitle'
-import { ArrowLeft, ArrowRight } from 'lucide-react'
-import { useState } from 'react'
-const Dialog = ({ img, url, title, text, body, github, dialogRef, close, slide, id }: ProjectType) => {
+"use client";
+import { ProjectType } from "@/app/utils/type";
+import { FaGithubSquare } from "react-icons/fa";
+import DialogTitle from "./DialogTitle";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useState } from "react";
+const Dialog = ({
+  img,
+  url,
+  title,
+  text,
+  body,
+  github,
+  dialogRef,
+  close,
+  slide,
+  id,
+}: ProjectType) => {
   const [src, setSrc] = useState<null | string>(slide ? slide[0] : null);
   const [currentIndex, setCurrentIndex] = useState(0);
   const next = () => {
@@ -13,25 +23,33 @@ const Dialog = ({ img, url, title, text, body, github, dialogRef, close, slide, 
       let newIndex = (prevIndex + 1) % slide?.length;
       setSrc(slide ? slide[newIndex] : null);
       return newIndex;
-    })
-  }
+    });
+  };
   const prev = () => {
     setCurrentIndex((previousIndex) => {
       let prevIndex = (previousIndex - 1 + slide?.length) % slide?.length;
       setSrc(slide ? slide[prevIndex] : null);
       return prevIndex;
-    })
-  }
+    });
+  };
   return (
     <dialog ref={dialogRef} className="w-full rounded">
       <article className="bg-primary-white rounded-lg shadow-md hover:shadow-2xl duration-300">
         <DialogTitle close={close} />
         <div className="p-4 border-b grid lg:grid-cols-2">
           <div className="capitalize p-8">
-            <h2 className="text-4xl lg:text-6xl tracking-wide font-medium">{title}</h2>
-            <p className="mt-4 text-slate-700 leading-loose">{body ? body : text}</p>
+            <h2 className="text-4xl lg:text-6xl tracking-wide font-medium">
+              {title}
+            </h2>
+            <p className="mt-4 text-slate-700 leading-loose">
+              {body ? body : text}
+            </p>
           </div>
-          <img src={img} alt={title} className="w-full object-cover rounded-t-lg h-64" />
+          <img
+            src={img}
+            alt={title}
+            className="w-full object-cover rounded-t-lg h-64"
+          />
         </div>
         <section className="p-4 w-full lg:w-3/4">
           <p>slide: {slide ? src : "no slide images"}</p>
@@ -50,10 +68,9 @@ const Dialog = ({ img, url, title, text, body, github, dialogRef, close, slide, 
             </button>
           </div>
         </section>
-
       </article>
     </dialog>
-  )
-}
+  );
+};
 
 export default Dialog;
--- src/app/utils/type.ts
+++ src/app/utils/type.ts
@@ -1,25 +1,27 @@
-import { ReactElement } from "react"
+import { ReactElement, RefObject } from "react";
 
 export type NavLinkType = {
-  id: string,
-  href: string,
-  text: string
-}
+  id: string;
+  href: string;
+  text: string;
+};
 
 export type SkillsType = {
-  id: string,
-  title: string,
-  icon: ReactElement,
-  text: string
-}
+  id: string;
+  title: string;
+  icon: ReactElement;
+  text: string;
+};
 
 export type ProjectType = {
-  id: string,
-  img: string,
-  url: string,
-  github: string,
-  title: string,
-  text: string,
-  body?: string
-  slide?: string[]
-}
+  id: string;
+  img: string;
+  url: string;
+  github: string;
+  title: string;
+  text: string;
+  body?: string;
+  slide?: string[];
+  dialogRef: RefObject<HTMLDialogElement>;
+  close: () => void;
+};
